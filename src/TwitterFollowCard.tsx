f3a8c31dcb92ff3555944279bcc098d83add8521
sgutierrezg <segut10@gmail.com>
2023-01-18 13:52:23 -0500

    subiendo mini proyecto

---
+++ src/TwitterFollowCard.tsx
@@ -0,0 +1,48 @@
+import React, { FC, useState } from 'react'
+import './App.css'
+
+interface TwitterInfo {
+    userName: string;
+    children: string;
+    initialIsFollowing: boolean;
+}
+
+
+const TwitterFollowCard: FC<TwitterInfo> = ({ children, userName, initialIsFollowing }) => {
+
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+    console.log('[TwitterFollowCard] render with userName: ', userName)
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button is-following'
+        : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img
+                    className='tw-followCard-avatar'
+                    alt="El avatar de midudev"
+                    src={`https://unavatar.io/${userName}`} />
+                <div className="tw-followCard-info">
+                    <strong>{children}</strong>
+                    <span className="tw-followCard-infoUserName">@{userName}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+                </button>
+            </aside>
+        </article>
+    )
+}
+
+export default TwitterFollowCard
